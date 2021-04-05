a8bc63a0167120d0968ff5a008ce4480c228b09b
finabluma <oscar.francino@finabluma.com>
2021-04-05 16:26:16 +0200

    bug fixed in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { useState, useEffect } from 'react'
 import { useAuth } from '../hooks/useAuth'
-import { getAllPosts } from '../lib/posts'
+import { getAllPosts, createPost } from '../lib/posts'
 import styles from '../styles/Home.module.scss'
 import Post from '../components/Post'
 import PostForm from '../components/PostForm'
@@ -20,10 +20,7 @@ export default function Home({ posts: defaultPost }) {
   async function handleOnSubmit(data, e) {
     e.preventDefault()
 
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/posts`, {
-      method: 'POST',
-      body: JSON.stringify(data)
-    })
+    await createPost(data)
 
     const posts = await getAllPosts()
     updatePosts(posts)
@@ -37,8 +34,8 @@ export default function Home({ posts: defaultPost }) {
       </Head>
 
 
-      {!user && <p><button onClick={logIn}>Log In</button></p>}
-      {user && <p><button onClick={logOut}>Log Out</button></p>}
+      {!user && (<p><button onClick={logIn}>Log In</button></p>)}
+      {user && (<p><button onClick={logOut}>Log Out</button></p>)}
 
       <main className={styles.main}>
         <Bio
@@ -64,7 +61,7 @@ export default function Home({ posts: defaultPost }) {
           })}
         </ul>
 
-        {user && <PostForm onSubmit={handleOnSubmit} />}
+        {user && (<PostForm onSubmit={handleOnSubmit} />)}
 
       </main>
     </div>
